6c8d41b8f8af0a656578d43111ffbecd8ae81032
Egor Oleynik <egor.slam@gmail.com>
2015-04-17 15:12:27 +0300

    Addes small version for avatars and many more.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ActiveRecord::Base
 	 after_create :add_new_blog
 	 mount_uploader :avatar, AvatarUploader
 
-	 validates_presence_of   :avatar
- 	 validates_integrity_of  :avatar
-  	 validates_processing_of :avatar
+	 #validates_presence_of   :avatar
+ 	 #validates_integrity_of  :avatar
+  	 #validates_processing_of :avatar
 
 	  def add_new_blog
 	    build_blog({title: "Noname blog"})
 	  end
 
 	  def full_name
-	  	current_user.first_name
+	  	"#{first_name} #{last_name}"
 	  end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,12 +17,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  
+    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -34,6 +35,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
  
      process :resize_to_fill => [200, 200]
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
+
+  version :small do
+    process :resize_to_fit => [50, 50]
+  end
+
+  version :medium do
+    process :resize_to_fit => [100, 100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
